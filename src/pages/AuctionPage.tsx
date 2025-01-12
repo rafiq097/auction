12054ae849b75741a41c07cd2150df181dfb8aa5
Rafiq Shaik <rafiqshaik097@gmail.com>
2025-01-12 09:43:47 +0530

    1234

--- src/pages/AuctionPage.tsx
+++ src/pages/AuctionPage.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { NavigateFunction, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 const AuctionPage: React.FC = () => {
-  const navigate: NavigateFunction = useNavigate();
+  const navigate = useNavigate();
   const [team, setTeam] = useState<string>("");
 
   useEffect(() => {
@@ -16,7 +16,105 @@ const AuctionPage: React.FC = () => {
     }
   }, []);
 
-  return <div>Current Team: {team.toLocaleUpperCase()}</div>;
+  return (
+    <>
+      {/* <div className="text-center text-lg font-bold mb-4">
+        Current Team: {team.toLocaleUpperCase()}
+      </div> */}
+      <div className="grid grid-rows-2 h-screen">
+        <div className="grid grid-cols-3">
+          {/* // Purse bros */}
+          <div className="bg-gray-100 h-full overflow-y-auto p-1">
+            <ul className="list-none space-y-1">
+            <li className="flex justify-between items-center border-b border-gray-300">
+                <span>Team</span>
+                <span>Spent</span>
+                <span>Remaining</span>
+              </li>
+              <li className="flex justify-between items-center border-b border-gray-300">
+                <span>RCB</span>
+                <span>00CR</span>
+                <span>120CR</span>
+              </li>
+              <li className="flex justify-between items-center border-b border-gray-300">
+                <span>MI</span>
+                <span>00CR</span>
+                <span>120CR</span>
+              </li>
+              <li className="flex justify-between items-center border-b border-gray-300">
+                <span>CSK</span>
+                <span>00CR</span>
+                <span>120CR</span>
+              </li>
+              <li className="flex justify-between items-center border-b border-gray-300">
+                <span>DC</span>
+                <span>00CR</span>
+                <span>120CR</span>
+              </li>
+              <li className="flex justify-between items-center border-b border-gray-300">
+                <span>KKR</span>
+                <span>00CR</span>
+                <span>120CR</span>
+              </li>
+              <li className="flex justify-between items-center border-b border-gray-300">
+                <span>SRH</span>
+                <span>00CR</span>
+                <span>120CR</span>
+              </li>
+              <li className="flex justify-between items-center border-b border-gray-300">
+                <span>RR</span>
+                <span>00CR</span>
+                <span>120CR</span>
+              </li>
+              <li className="flex justify-between items-center border-b border-gray-300">
+                <span>PBKS</span>
+                <span>00CR</span>
+                <span>120CR</span>
+              </li>
+              <li className="flex justify-between items-center border-b border-gray-300">
+                <span>LSG</span>
+                <span>00CR</span>
+                <span>120CR</span>
+              </li>
+              <li className="flex justify-between items-center border-b border-gray-300">
+                <span>GT</span>
+                <span>00CR</span>
+                <span>120CR</span>
+              </li>
+            </ul>
+          </div>
+          
+          {/* // Player Info for Bid */}
+          <div className="bg-red-200 h-full flex items-center justify-center">
+            2
+          </div>
+
+          {/* // User Team Information */}
+          <div className="bg-red-300 h-full flex items-center justify-center">
+            3
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3">
+
+          {/* // Teams Bid Status */}
+          <div className="bg-blue-100 h-full flex items-center justify-center">
+            4
+          </div>
+
+          {/* // Current Bid */}
+          <div className="bg-blue-200 h-full flex items-center justify-center">
+            5
+          </div>
+
+          {/* // Recent */}
+          <div className="bg-blue-300 h-full flex items-center justify-center">
+            6
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AuctionPage;
