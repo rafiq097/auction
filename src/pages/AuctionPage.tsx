84705660f9f6a3c05d7bfa4073b3ffe95d3c9e83
Rafiq Shaik <rafiqshaik097@gmail.com>
2025-01-13 20:04:19 +0530

    aut

--- src/pages/AuctionPage.tsx
+++ src/pages/AuctionPage.tsx
@@ -36,6 +36,27 @@ const AuctionPage: React.FC = () => {
     price: number;
     team: string;
   }>();
+  const [biddingBros, setBiddingBros] = useState<
+    {
+      name: string;
+      bid: number;
+    }[]
+  >([
+    { name: "RCB", bid: 0 },
+    { name: "MI", bid: 0 },
+    { name: "CSK", bid: 0 },
+    { name: "DC", bid: 0 },
+    { name: "KKR", bid: 0 },
+    { name: "SRH", bid: 0 },
+    { name: "RR", bid: 0 },
+    { name: "PBKS", bid: 0 },
+    { name: "GT", bid: 0 },
+    { name: "LSG", bid: 0 },
+  ]);
+  const [currentBid, setCurrentBid] = useState<{
+    name: string;
+    bid: number;
+  }>();
 
   useEffect(() => {
     const curr = localStorage.getItem("team");
@@ -107,6 +128,43 @@ const AuctionPage: React.FC = () => {
       updatedPlayers[curr].base = price;
       return updatedPlayers;
     });
+
+    setCurrentBid({ name: team.name, bid: price });
+    let num = Math.floor(Math.random() * teams.length);
+    while(teams[num].name != team.name && teams[num].remaining >= price) {
+      num = Math.floor(Math.random() * teams.length);
+    }
+
+    price += 2000000;
+    setPlayers((prevPlayers) => {
+      const updatedPlayers = [...prevPlayers];
+      updatedPlayers[curr].base = price;
+      return updatedPlayers;
+    });
+    setTimeout(() => setCurrentBid({ name: teams[num].name, bid: price }), 2000);
+    
+    // const updatedTeams = teams.map((team, index) => {
+    //   if (index === num) {
+    //     const newSpent = team.spent + price;
+    //     const newRemaining = team.remaining - price;
+
+    //     return {
+    //       ...team,
+    //       spent: newSpent,
+    //       remaining: newRemaining,
+    //       players: [...team.players, { ...players[curr], price }],
+    //     };
+    //   }
+    //   return team;
+    // });
+
+    // setTeams(updatedTeams);
+  };
+
+  const handleReset = (): void => {
+    localStorage.removeItem("team");
+    localStorage.removeItem("teams");
+    window.location.href = "/";
   };
 
   return (
@@ -117,7 +175,6 @@ const AuctionPage: React.FC = () => {
       {console.log(teams)}
       <div className="grid grid-rows-2 h-screen">
         <div className="grid grid-cols-3">
-
           {/* // Purse bros */}
           <div className="bg-gray-100 h-full overflow-y-auto p-1">
             <ul className="list-none space-y-1">
@@ -141,8 +198,16 @@ const AuctionPage: React.FC = () => {
 
           {/* // Player bros Bid */}
           <div className="bg-gray-300 h-full flex items-center justify-center">
-            <div className="p-10 bg-gray-50 rounded-lg shadow-lg max-w-lg">
-              <h1 className="text-2xl font-bold mb-4">{players[curr].type}</h1>
+            <div className="p-6 bg-gray-50 rounded-lg shadow-lg max-w-lg">
+              <div className="flex justify-center">
+                <button
+                  className="px-6 py-2 bg-red-500 text-white font-semibold rounded-md hover:bg-red-600 transition duration-300 mb-2"
+                  onClick={handleReset}
+                >
+                  Reset Auction
+                </button>
+              </div>
+              <h1 className="text-2xl font-bold mb-2">{players[curr].type}</h1>
               <h2 className="text-xl font-semibold mb-2">
                 {players[curr].name}
               </h2>
@@ -172,7 +237,23 @@ const AuctionPage: React.FC = () => {
           </div>
 
           {/* // Teams bros Bid Status */}
-          <div className="bg-gray-100 h-full">Bidding Status</div>
+          <div className="bg-gray-100 h-full">
+            <ul className="list-none space-y-1 ml-2 mr-4">
+              <li className="bg-gray-200 flex justify-between items-center border-b border-gray-300">
+                <span>Team</span>
+                <span>Bid</span>
+              </li>
+              {biddingBros.map((team) => (
+                <li
+                  key={team.name}
+                  className="flex justify-between items-center border-b border-gray-300"
+                >
+                  <span>{team.name}</span>
+                  <span>{team.bid.toFixed(1)}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
 
         <div className="grid grid-cols-3">
@@ -197,15 +278,29 @@ const AuctionPage: React.FC = () => {
           </div>
 
           {/* // Bid war */}
-          <div className="bg-gray-100 h-full flex items-center justify-center">
-            6
+          <div className="bg-gray-100 h-full flex flex-col items-center justify-center p-4 rounded-lg shadow-md max-w-sm mx-auto">
+            <h2 className="text-2xl font-bold text-gray-800 mb-4">
+              Current Bid
+            </h2>
+            <div className="text-center">
+              {currentBid ? (
+                <>
+                  <p className="text-4xl text-green-600 font-extrabold">
+                    ₹{CR(currentBid.bid).toFixed(1)}
+                  </p>
+                  <p className="text-lg text-red-700 font-medium mt-2">
+                    Bidder: <span className="font-bold">{currentBid.name}</span>
+                  </p>
+                </>
+              ) : (
+                <p className="text-xl text-gray-600 italic">Start Bidding!</p>
+              )}
+            </div>
           </div>
 
           {/* // User Team Information */}
           <div className="bg-gray-300 h-full">
-            <h1 className="text-xl font-bold mb-4">
-              Your Team: {team.name}
-            </h1>
+            <h1 className="text-xl font-bold mb-4">Your Team: {team.name}</h1>
             <ul className="rounded">
               <li className="mb-2 text-gray-700 font-medium">
                 Batters: {team.batters}
