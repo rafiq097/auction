3f3fb7bf3fec34b8965a35967beccd14b5d357bc
Rafiq Shaik <rafiqshaik097@gmail.com>
2025-01-22 15:08:55 +0530

    login bros

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,10 +7,14 @@ import TeamsPage from "./pages/TeamsPage";
 import toast, { Toaster } from "react-hot-toast";
 import { userTeamAtom } from "./atoms/userTeamAtom.ts";
 import { useRecoilState } from "recoil";
+import userAtom from "./atoms/userAtom.ts";
+import LoginPage from "./pages/LoginPage.tsx";
+import axios from "axios";
 
 function App(): JSX.Element {
   const navigate: NavigateFunction = useNavigate();
   const [, setTeam] = useRecoilState(userTeamAtom);
+  const [userData, setUserData] = useRecoilState(userAtom);
 
   useEffect(() => {
     const curr = localStorage.getItem("team");
@@ -23,13 +27,39 @@ function App(): JSX.Element {
     }
   }, []);
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios
+        .get("/verify", {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+          setUserData(res.data.user);
+          navigate("/");
+        })
+        .catch((err) => {
+          console.log(err.message);
+          localStorage.removeItem("token");
+          setUserData(null);
+        });
+    }
+  }, [setUserData, navigate]);
+
   return (
     <>
       <Toaster />
       <Routes>
-        <Route path="/" element={<StartPage />} />
-        <Route path="/auction" element={<AuctionPage />} />
-        <Route path="/teams" element={<TeamsPage />} />
+        <Route path="/" element={!userData ? <LoginPage /> : <StartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/auction"
+          element={!userData ? <LoginPage /> : <AuctionPage />}
+        />
+        <Route
+          path="/teams"
+          element={!userData ? <LoginPage /> : <TeamsPage />}
+        />
       </Routes>
     </>
   );
---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,81 @@
+import { useEffect } from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRecoilState } from "recoil";
+import { useNavigate } from "react-router-dom";
+import userAtom from "../atoms/userAtom";
+
+function LoginPage(): JSX.Element {
+  const [, setUserData] = useRecoilState(userAtom);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios
+        .get("/verify", {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+          setUserData(res.data.user);
+          navigate("/");
+        })
+        .catch((err) => {
+          console.log(err.message);
+          localStorage.removeItem("token");
+          setUserData(null);
+        })
+    }
+  }, [setUserData, navigate]);
+
+  const handleLogin = async (x: { email: any; name: any; }) => {
+    try {
+      console.log("login called");
+
+      const res = await axios.post("/users/login", {
+        email: x.email,
+        name: x.name,
+      });
+
+      console.log(res);
+      localStorage.setItem("token", res.data.token);
+      setUserData(res.data.user);
+
+      toast.success("Login Success");
+      navigate("/");
+    } catch (err: any) {
+      console.log(err.message);
+      console.log(err);
+      toast.error("Please try again...");
+    }
+  };
+
+  return (
+    <>
+      <div className="h-screen flex">
+        <div className="w-full bg-gray-800 flex items-center justify-center p-8">
+          <div className="text-center">
+            {(
+              <>
+                <GoogleLogin
+                  onSuccess={(res: { credential: any; }) => {
+                    let x: any = jwtDecode(res?.credential);
+                    handleLogin(x);
+                  }}
+                  onError={(err: any) => {
+                    console.log(err, "Login Failed");
+                  }}
+                />
+                <p className="text-white mt-4">Sign in with Google</p>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LoginPage;
