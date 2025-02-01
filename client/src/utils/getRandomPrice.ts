9fd6fba1ce06a2a96edc8923036cf99c66f78ce0
Rafiq Shaik <rafiqshaik097@gmail.com>
2025-02-01 15:37:12 +0530

    randomprice

--- client/src/utils/getRandomPrice.ts
+++ client/src/utils/getRandomPrice.ts
@@ -29,8 +29,11 @@ export const getRandomPrice = (player: {
 
   if (randomPrice > 40) randomPrice *= 0.5;
   if (randomPrice > 30) randomPrice *= 0.7;
+  if (randomPrice > 20) randomPrice *= 0.8;
   if (randomPrice > 20) randomPrice *= 0.9;
-  if (randomPrice > 20) randomPrice *= 0.9;
+  if (randomPrice > 20) randomPrice *= 0.95;
+  if (randomPrice <= 15) randomPrice *= 0.8;
+  if (randomPrice <= 10) randomPrice *= 0.9;
 
   randomPrice = Math.round(randomPrice * 4) / 4;
   randomPrice = parseFloat(randomPrice.toFixed(2));
